b7fddca787ae1d5c332f330388a9232e889fed0b
ericnakagawa <ericnakagawa@users.noreply.github.com>
2020-12-01 15:00:28 +0000

    deploy: 45dcd3e4c868b2cf50cc252fc2a3a688adaf1e45

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["libra_canonical_serialization"] = {"name":"","files":["de.rs","error.rs","lib.rs","ser.rs","test_helpers.rs"]};
+createSourceSidebar();
